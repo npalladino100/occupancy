3fb24a1e0828e8db4d63f777b8258ed97ab0d233
Nick Palladino <npallad@bu.edu>
2020-11-29 20:46:43 -0500

    Updated file location
    New Script

--- ESP8266 Programs/A-Wi-Fi_connection/A-Wi-Fi_connection.ino
+++ ESP8266 Programs/A-Wi-Fi_connection/A-Wi-Fi_connection.ino
@@ -1,28 +1,22 @@
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WiFi.h>
 
-const char* ssid     = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "PASSWORD";     // The password of the Wi-Fi network
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
 
-void setup() {
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
-  delay(10);
-  Serial.println('\n');
-  
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
+  WiFi.begin("Best wifi", "Moniterrier");
 
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print(++i); Serial.print(' ');
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println();
 
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() { }
-//12345
+void loop() {}
